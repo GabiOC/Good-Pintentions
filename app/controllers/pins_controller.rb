432088949244d2274fb448bdb33cc2bd07277841
Gabriele O'Connor <gabrieleoconnor@Gabis-MacBook-Pro.local>
2014-11-09 15:52:26 -0500

    Cleaned up the pins scaffold

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,49 @@
+class PinsController < ApplicationController
+  respond_to :html, :xml, :json
+  before_action :set_pin, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @pins = Pin.all
+    respond_with(@pin)
+  end
+
+  def show
+    respond_with(@pin)
+  end
+
+  def new
+    @pin = Pin.new
+    respond_with(@pin)
+  end
+
+  def edit
+  end
+
+  def create
+    @pin = Pin.new(pin_params)
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    @pin.update(pin_params)
+    respond_with(@pin)
+  end
+
+  def destroy
+    @pin.destroy
+    respond_with(@pin)
+  end
+
+  private
+    def set_pin
+      @pin = Pin.find(params[:id])
+    end
+
+    def pin_params
+      params.require(:pin).permit(:description)
+    end
+end
